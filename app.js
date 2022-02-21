a8f1e8633d063fedc1b7c676601c0429235a56b0
Sahara <THsahara@gmail.com>
2022-02-21 15:19:33 -0500

    added const App  and Header 2 function

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 function Header() {
-    const Header1 = "World Wide Web"
         return(
             <div>
                 <h1>World Wide Web</h1>
@@ -7,4 +6,24 @@ function Header() {
                     Everything there is online about W3 is linked directly or indirectly to this document, including and executive summary or the project, Mailing lists, Policy, November's W3 News, and Frequently Asked Questions.</p>
             </div>
         );
+}
+function Header2() {
+    return(
+        <div>
+            <h3>What's Out there?</h3>
+            <p>Pointers to the World's online information,subjects,W3,servers,etc.</p>
+        </div>
+    )
+}
+
+
+
+
+
+const App = () => {
+    return (
+        <div>
+            <Header>
+        </div>
+    )
 }
