ee5fc94d5d02eeb499fa0ffe84f77d7b957fe8cd
Sahara <THsahara@gmail.com>
2022-02-21 15:28:05 -0500

    added Header 3

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ function Header() {
         return(
             <div>
                 <h1>World Wide Web</h1>
-                <p>The WorldWide Web (W3) is a wide area hypermedia information retrieval initiative aiming to give universal access to a large universe of documents.
-                    Everything there is online about W3 is linked directly or indirectly to this document, including and executive summary or the project, Mailing lists, Policy, November's W3 News, and Frequently Asked Questions.</p>
+                <para1>The WorldWide Web (W3) is a wide area hypermedia information retrieval initiative aiming to give universal access to a large universe of documents.
+                    Everything there is online about W3 is linked directly or indirectly to this document, including and executive summary or the project, Mailing lists, Policy, November's W3 News, and Frequently Asked Questions.</para1>
             </div>
         );
 }
@@ -11,7 +11,16 @@ function Header2() {
     return(
         <div>
             <h3>What's Out there?</h3>
-            <p>Pointers to the World's online information,subjects,W3,servers,etc.</p>
+            <para2>Pointers to the World's online information,subjects,W3,servers,etc.</para2>
+        </div>
+    )
+}
+
+function Header3() {
+    return(
+        <div>
+            <h3>Help</h3>
+            <para3>on the browser you're using</para3>
         </div>
     )
 }
@@ -23,7 +32,9 @@ function Header2() {
 const App = () => {
     return (
         <div>
-            <Header>
+            <Header/>
+            <Header2/>
+            <Header3/>
         </div>
     )
 }
