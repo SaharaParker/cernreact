e29cd21ecc4171c015fd16c1654306ee3946f78e
Sahara <THsahara@gmail.com>
2022-02-21 14:56:28 -0500

    added app.js and added function to app.js

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-const Header = () => {
-    return(
-        <div>
-
-        </div>
-    );
-};
+function Header() {
+    const Header1 = "World Wide Web"
+        return(
+            <div>
+                <h1>World Wide Web</h1>
+                <p>The WorldWide Web (W3) is a wide area hypermedia information retrieval initiative aiming to give universal access to a large universe of documents.
+                    Everything there is online about W3 is linked directly or indirectly to this document, including and executive summary or the project, Mailing lists, Policy, November's W3 News, and Frequently Asked Questions.</p>
+            </div>
+        );
+}
